19b9182e8ddd308c5a74f7c66805afb6225ebd3b
golden22a <golden-boy1994@hotmail.fr>
2018-03-30 18:38:41 -0400

    redirect after log in

--- src/App.js
+++ src/App.js
@@ -10,28 +10,34 @@ import Login from './component/Login';
 import Connect from './auth/connect';
 import Nav from './component/Nav';
 class App extends Component {
-  constructor(){
-    super()
+  constructor(props){
+    super(props);
     this.state={
-      connect:false,
+      connected:false,
+      token:'',
       user:{}
     }
     this.login=this.login.bind(this);
   }
   componentWillMount(){
+    if(!this.state.connected){
+      console.log('calling');
     let token=localStorage.getItem('token');
     Connect.islogged(token).then((res)=>{
       this.setState({
         connected:true,
-        user:res.data.user
+        user:res.data.user,
+        token:res.data.token
       })
     }).catch( (err) => {
       console.log(err);
     })
   }
+  }
   login(token,user){
     this.setState({
       connected:true,
+      token:token,
       user:user
     });
     localStorage.setItem('token', token);
@@ -39,12 +45,16 @@ class App extends Component {
 
   }
   render() {
-    let logged = !this.state.connected ? < Login  login={this.login}/> : null ;
     return (
       <div className='main'>
       <Nav connected={this.state.connected} user={this.state.user}/>
       <div className="container">
-      {logged}
+
+      <Switch>
+        <Route path="/login" render={(props) => <Login {...props} login={this.login} connected={this.state.connected}/>} />
+
+      </Switch>
+
       </div>
       </div>
     );
