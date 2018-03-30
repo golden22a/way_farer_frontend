664beaa0ef5bd290f42c97fedc2c3e153814d798
fchow2018 <dong_chow@yahoo.com>
2018-03-30 15:44:10 -0700

    added home page

--- src/App.js
+++ src/App.js
@@ -7,22 +7,46 @@ import {
     Switch
 } from 'react-router-dom';
 import Login from './component/Login';
+import Connect from './auth/connect';
+import Nav from './component/Nav';
+import Home from './component/Home';
 class App extends Component {
   constructor(){
     super()
     this.state={
-      token:''
+      connect:false,
+      user:{}
     }
     this.login=this.login.bind(this);
   }
-  login(email,password){
-    console.log(email);
-    console.log(email);
+  componentWillMount(){
+    let token=localStorage.getItem('token');
+    Connect.islogged(token).then((res)=>{
+      this.setState({
+        connected:true,
+        user:res.data.user
+      })
+    }).catch( (err) => {
+      console.log(err);
+    })
+  }
+  login(token,user){
+    this.setState({
+      connected:true,
+      user:user
+    });
+    localStorage.setItem('token', token);
+    console.log(this.state.token);
+
   }
   render() {
+    let logged = !this.state.connected ? < Login  login={this.login}/> : null ;
     return (
+      <div className='main'>
+      <Nav connected={this.state.connected} user={this.state.user}/>
       <div className="container">
-      <Login login={this.login}/>
+      {logged}
+      </div>
       </div>
     );
   }
---
+++ src/component/Nav.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import {Navbar, NavItem} from 'react-materialize';
+
+class Nav extends Component {
+  render() {
+  let el= !this.props.connected ? (<ul>
+    <li>
+      <NavItem href="/login">Sign In</NavItem>
+    </li>
+    <li>
+      <NavItem href="/signup">Sign Up</NavItem>
+    </li>
+  </ul>) : (<ul>
+    <li>
+      <NavItem  href="/profile">{this.props.user.firstname} </NavItem>
+    </li>
+    <li>
+      <NavItem href="/posts">Posts</NavItem>
+    </li>
+  </ul>)
+    return (
+<Navbar class="nav" brand='logo' right>
+  {el}
+</Navbar>
+)}
+}
+
+export default Nav;
