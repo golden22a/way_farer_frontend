ee5c61dd884e5d0d42e640d308da361d9e531fbf
juancarlucci <juancarloscollins@gmail.com>
2018-04-01 13:20:41 -0700

    fix city conflict

--- src/component/City.js
+++ src/component/City.js
@@ -1,25 +1,12 @@
 import React, {Component} from 'react'
-<<<<<<< HEAD
-import '../CitiesList.css';
-=======
 import {Col,Card} from 'react-materialize';
 
->>>>>>> 0d49a711413ba8bb66c34ca57e2288f651a8d61b
 
 class City extends Component {
 
   render(){
 
       return (
-<<<<<<< HEAD
-      <li className="city">
-        <div class="card">
-          <img src="https://placehold.it/100x100" />
-         <h4>{this.props.city.name}</h4>
-
-       </div>
-     </li>
-=======
         <Col s={12}>
         <li>
 
@@ -29,7 +16,6 @@ class City extends Component {
 </Col>
 
 
->>>>>>> 0d49a711413ba8bb66c34ca57e2288f651a8d61b
       )
   }
 }
