62becb40edbbde619caf4ae0d41a4291e18c2b32
fchow2018 <dong_chow@yahoo.com>
2018-04-02 08:58:57 -0700

    played around with carousel

--- src/component/Home.js
+++ src/component/Home.js
@@ -9,35 +9,37 @@
     render() {
       return (
         <div className='container'>
-          <Row>
-          <Col l={12} s={12}>
+        <Row>
+        <Col l={14} s={12}>
 
-                  <Carousel options={{ fullWidth: true }} images={[
-            'https://i.ytimg.com/vi/oi2o2r3Sgc0/maxresdefault.jpg',
-            'https://www.telegraph.co.uk/content/dam/Travel/Destinations/North%20America/USA/California/los%20angeles/Los%20Angeles%20lead-xlarge.jpg',
-            'https://www.acg.org/sites/files/styles/masthead_1440/public/acg-seattle-2880x1200.jpg?itok=BSAs0Uxl',
-            'https://travelnoire.com/wp-content/uploads/2014/12/o-NEW-YORK-CITY-WRITER-facebook.jpg',
-            'https://saintpaul.s3.amazonaws.com/CMS/1884/saint-paul-skyline-vsp__large-slideshow.jpg'
-          ]} />
+                <Carousel options={{ fullWidth: true }} images={[
+          'https://i.ytimg.com/vi/oi2o2r3Sgc0/maxresdefault.jpg',
+          'https://www.telegraph.co.uk/content/dam/Travel/Destinations/North%20America/USA/California/los%20angeles/Los%20Angeles%20lead-xlarge.jpg',
+          'https://media.apts247.info/5b/5bb81c29ec194f37bc66234964c56082/amenity_lists/community-amenities.jpg',
+          'https://az616578.vo.msecnd.net/files/2016/02/28/635922165286429907739493334_NYC.jpg',
+          'https://saintpaul.s3.amazonaws.com/CMS/1884/saint-paul-skyline-vsp__large-slideshow.jpg'
+        ]} />
+
+        </Col>
+        </Row>
 
 
-          </Col>
-          </Row>
+
           <div>
           <h4>Wayfarer is...</h4>
           </div>
           <Row className='articles'>
           <Col s={4} >
           <h3>Friendly</h3>
-          <p>Wonderful, helpful community. Excellent customer service.</p>
+          <p>We strive to build a wonderful, helpful community and are constantly looking for ways to improve customer service.</p>
           </Col>
           <Col s={4} >
           <h3>Informative</h3>
-          <p>A community filled with outstanding knowledge.</p>
+          <p>We are determined to build a community filled with knowledgeable people, so that we can help provide the most useful tips to other travelers.</p>
           </Col>
           <Col s={4} >
           <h3>Fun</h3>
-          <p>Fun to use and learn useful tips about your favorite locations.</p>
+          <p>We aim to make the Wayfarer website fun to use, so that customers can bond with others about their favorite locations.</p>
           </Col>
           </Row>
         </div>
